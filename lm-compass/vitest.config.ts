c73fbf3ded863525c3ccea61745a4ee59ae1871b
Aadi Sanghani <aadisanghani04@gmail.com>
2026-03-21 14:42:30 -0400

    add infra for tests

---
+++ lm-compass/vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    environment: "jsdom",
+    passWithNoTests: true,
+    setupFiles: ["./vitest.setup.ts"],
+    include: ["**/*.{test,spec}.{ts,tsx}"],
+    exclude: [
+      "**/node_modules/**",
+      "**/.next/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/coverage/**",
+      "**/.{idea,git,cache,output,temp}/**",
+    ],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+    },
+  },
+});
